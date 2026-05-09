95f600e3bbb180cc635714a7e862ac3aaf8718dc
Arrowar <62809003+Arrowar@users.noreply.github.com>
2026-05-09 10:23:11 +0200

    [RELEASE] Version 1.2.3

--- VibraVid/upload/version.py
+++ VibraVid/upload/version.py
@@ -1,5 +1,5 @@
 __title__ = 'VibraVid'
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 __author__ = 'AstraeLabs'
 __description__ = 'A command-line program to download film'
 __copyright__ = 'Copyright 2026'
